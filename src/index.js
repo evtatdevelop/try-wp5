d91ceadab1c59ea5c3dc6a29b457cc56e38d8a16
Evgenii <evtatdevelop@gmail.com>
2021-01-05 22:42:37 +0700

    Add CSS, IMG, Font

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from "lodash";
+import "./style.css";
+import Kitty from "./kitty.jpg";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  // _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // Add the image to our new div
+  // kittyImg.classList.add("kitty");
+  const myKitty = new Image();
+  myKitty.src = Kitty;
+  myKitty.classList.add("kitty");
+  element.appendChild(myKitty);
 
   return element;
 }
